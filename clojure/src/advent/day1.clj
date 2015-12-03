2649f2b728ce3ab1e88f0f6ccde36e69c86558de
Albin Theander <albin.theander@sonymobile.com>
2015-12-03 17:20:03 +0100

    Clojure Day1.2

--- clojure/src/advent/day1.clj
+++ clojure/src/advent/day1.clj
@@ -4,10 +4,21 @@
 
 (defn final-floor [steps]
 	(->> steps
-		 (map {\( 1, \) -1})
-		 (reduce +)))
+		 (map UP_DOWN)
+		 (reduce + 0)))
+
+(defn index-of-basement-step [steps]
+	(let [cnt (fn cnt [list sum depth]
+  				(cond
+          			(< sum 0) depth
+          			(empty? list) -1
+          			:else (recur (rest list) (+ sum (first list)) (inc depth))))]
+		(cnt (map UP_DOWN steps) 0 0)))
 
 (defn day1 []
 	(let [steps (slurp "../data/day1.txt")]
-		(println "Santa ends up at floor " (final-floor steps))))
+		(do
+			(println "Santa ends up at floor" (final-floor steps))
+			(println "Santa enters the basement in step" (index-of-basement-step steps)))))
+
 
--- clojure/test/advent/day1_test.clj
+++ clojure/test/advent/day1_test.clj
@@ -11,3 +11,11 @@
   	(is (= -1 (final-floor ")"))))
   (testing "Can follow the elevator a few steps"
   	(is (= 3 (final-floor "(()(()(")))))
+
+(deftest index-of-basement-step-test
+	(testing "going down in the basement directly"
+		(= 1 (index-of-basement-step ")")))
+	(testing "going up a bit before plunging down"
+		(= 5 (index-of-basement-step "(()))")))
+	(testing "a bit of yo-yo action before basement"
+		(= 13 (index-of-basement-step "(()())((())))((()((()()"))))
